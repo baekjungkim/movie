bdc809cdfd30d6e2151c2083612782b25d2375e8
baekjung <zvgandam@gmail.com>
2019-01-28 18:12:05 +0900

    Updates

---
+++ precache-manifest.78461cbed758e513af57cd5c6d40ab74.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "00fbc40c4d7c4278f190",
+    "url": "/movie/static/css/main.00e77321.chunk.css"
+  },
+  {
+    "revision": "00fbc40c4d7c4278f190",
+    "url": "/movie/static/js/main.00fbc40c.chunk.js"
+  },
+  {
+    "revision": "d120fdf0ddba1865b329",
+    "url": "/movie/static/js/1.d120fdf0.chunk.js"
+  },
+  {
+    "revision": "31963bdd32f5110fe4df",
+    "url": "/movie/static/js/runtime~main.31963bdd.js"
+  },
+  {
+    "revision": "f7d833dd659c365298688684ec77c151",
+    "url": "/movie/index.html"
+  }
+];
